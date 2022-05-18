5c6dba41d322cbcf50f279165430de17b3c84d9c
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-05-18 16:45:59 +0530

    [TOMESHWAR]ADD:-ONE SPECIAL CHARACTOR SHOULD BE

--- RegexPattern/Program.cs
+++ RegexPattern/Program.cs
@@ -4,7 +4,7 @@ using RegexPattern;
 
 Pattern patterns = new Pattern();
 //Console.WriteLine(patterns.validatePinCode("xyz@bridgelabz.com.in.in.in"));
-if (patterns.validateString("Abdegftq1"))
+if (patterns.validateString("Abdegftq1@"))
 {
     Console.WriteLine("Valid");
 }
